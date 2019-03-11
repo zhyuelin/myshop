ea5b60f5b11a23ac31dd235b07c5a3e5840bdf5d
seven117 <13642777207@163.com>
2019-03-11 16:20:23 +0800

    重新提交aside

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,55 +1,77 @@
 <template>
-    <Menu active-name="1-2" :open-names="['1']" class="aside_container">
-        <div class="app-aside"></div>
-        <Submenu name="1">
-            <template slot="title">
-                <Icon type="ios-analytics" />
-                Navigation One
-            </template>
-            <MenuGroup title="Item 1">
-                <MenuItem name="1-1">Option 1</MenuItem>
-                <MenuItem name="1-2">Option 2</MenuItem>
-            </MenuGroup>
-            <MenuGroup title="Item 2">
-                <MenuItem name="1-3">Option 3</MenuItem>
-                <MenuItem name="1-4">Option 4</MenuItem>
-            </MenuGroup>
-        </Submenu>
-        <Submenu name="2">
-            <template slot="title">
-                <Icon type="ios-filing" />
-                Navigation Two
-            </template>
-            <MenuItem name="2-1">Option 5</MenuItem>
-            <MenuItem name="2-2">Option 6</MenuItem>
-            <Submenu name="3">
-                <template slot="title">Submenu</template>
-                <MenuItem name="3-1">Option 7</MenuItem>
-                <MenuItem name="3-2">Option 8</MenuItem>
-            </Submenu>
-        </Submenu>
-        <Submenu name="4">
-            <template slot="title">
-                <Icon type="ios-cog" />
-                Navigation Three
-            </template>
-            <MenuItem name="4-1">Option 9</MenuItem>
-            <MenuItem name="4-2">Option 10</MenuItem>
-            <MenuItem name="4-3">Option 11</MenuItem>
-            <MenuItem name="4-4">Option 12</MenuItem>
-        </Submenu>
-    </Menu>
+  <Menu active-name="1-2" :open-names="['1']" class="aside_container">
+    <div class="app-aside"></div>
+    <Submenu v-for="(item,index) in meaus" :key="index" :name="`${index+1}`">
+      <template slot="title">
+        <Icon :type="item.icon"/>
+        {{item.tile}}
+      </template>
+      <MenuGroup title="Item 1">
+        <MenuItem name="1-1">Option 1</MenuItem>
+        <MenuItem name="1-2">Option 2</MenuItem>
+      </MenuGroup>
+    </Submenu>
+  </Menu>
 </template>
 <script>
-    export default {
-        
-    }
+export default {
+  data() {
+    return {
+      menus: [
+        {
+          title: "购物商城",
+          path: "/admin",
+          icon: "ios-analytic",
+          group: [
+            {
+              path: "/goods-list",
+              title: "商品管理"
+            },
+            {
+              path: "/category-list",
+              title: "栏目管理"
+            },
+            {
+              path: "/comment-list",
+              title: "评论管理"
+            }
+          ]
+        },
+        {
+          title: "会员管理",
+          path: "/admin",
+          icon: "ios-filing",
+          group: [
+            {
+              path: "/account-list",
+              title: "会员列表"
+            }
+          ]
+        },
+        {
+          title: "商城订单",
+          path: "/admin",
+          icon: "ios-cog",
+          group: [
+            {
+              path: "/order-list",
+              title: "订单管理"
+            }
+          ]
+        }
+      ]
+    };
+  }
+};
 </script>
 <style>
-.app-aside{
-  height:64px; 
+.app-aside {
+  height: 64px;
 }
-.aside_container{
-  height: 100%
+.aside_container {
+  height: 100%;
+  width: 200px !important;
+  background: #001529;
+  color: #fff;
 }
 </style>
